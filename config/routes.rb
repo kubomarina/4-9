caf3e4ee01a4d10cdc2f3a60861f37a7f23ef4e0
kubmarina <sa1225no@gmail.com>
2021-04-10 14:50:48 +0000

    edit bookers2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy] 
+  
+  resources :users, only: [:show, :edit, :update,]
+  
+ 
+end
